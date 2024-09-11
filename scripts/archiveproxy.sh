d933d0487715610594d7feb8aff314b091c0d775
万里 <920113370213@njust.edu.cn>
2024-09-11 13:41:11 +0800

    Update archiveproxy.sh

--- scripts/archiveproxy.sh
+++ scripts/archiveproxy.sh
@@ -4,7 +4,7 @@ install_dir=$(which 1pctl | xargs grep '^BASE_DIR=' | cut -d'=' -f2)
 
 rm -rf $install_dir/1panel/resource/apps/local/napcat-1panel-napcat
 
-wget -O $install_dir/1panel/resource/apps/local/napcat-1panel-napcat.zip https://github.com/Fahaxikiii/napcat-1panel/archive/refs/heads/napcat.zip
+curl -o $install_dir/1panel/resource/apps/local/napcat-1panel-napcat.zip https://github.com/Fahaxikiii/napcat-1panel/archive/refs/heads/napcat.zip
 
 if [ $? -eq 0 ]; then
     unzip $install_dir/1panel/resource/apps/local/napcat-1panel-napcat.zip
