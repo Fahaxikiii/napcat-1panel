cfacddcec4ddcb001c491e8dc8ff81175ad2589e
Fahaxikiii <920113370213@njust.edu.cn>
2024-09-11 14:57:08 +0800

    update

--- 2.4.4/scripts/init.sh
+++ 2.4.4/scripts/init.sh
@@ -3,12 +3,6 @@
 source ./.env
 mkdir -p "${CONFIG_PATH}"
 touch "${CONFIG_PATH}/webui.json"
-sed -i "s/\"host\": \".*\"/\"host\": \"${WEBUI_HOST}\"/" ${CONFIG_PATH}/webui.json
-sed -i "s/\"port\": [0-9]*/\"port\": ${PANEL_APP_PORT_HTTP}/" ${CONFIG_PATH}/webui.json
-sed -i "s/\"prefix\": \".*\"/\"prefix\": \"${WEBUI_PREFIX}\"/" ${CONFIG_PATH}/webui.json
-sed -i "s/\"token\": \".*\"/\"token\": \"${WEBUI_TOKEN}\"/" ${CONFIG_PATH}/webui.json
-sed -i "s/\"loginRate\": [0-9]*/\"loginRate\": ${WEBUI_LOGIN_RATE}/" ${CONFIG_PATH}/webui.json
-
 WEBUI_PATH="${CONFIG_PATH}/webui.json"
 cat <<EOF > "$WEBUI_PATH"
 {
