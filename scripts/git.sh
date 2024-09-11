65cc9f97e0a366cf758281173297ef91c9c9ebab
万里 <920113370213@njust.edu.cn>
2024-09-11 13:55:05 +0800

    Update git.sh

--- scripts/git.sh
+++ scripts/git.sh
@@ -2,9 +2,9 @@
 
 install_dir=$(which 1pctl | xargs grep '^BASE_DIR=' | cut -d'=' -f2)
 
-rm -rf $install_dir/1panel/resource/apps/local/napcat-temp
+rm -rf $install_dir/1panel/resource/apps/local/napcat-1panel-napcat
 
-git clone -b napcat /https://github.com/Fahaxikiii/napcat-1panel.git "$install_dir/1panel/resource/apps/local/napcat-temp"
+git clone -b napcat /https://github.com/Fahaxikiii/napcat-1panel.git "$install_dir/1panel/resource/apps/local/napcat-1panel-napcat"
 
 if [ $? -eq 0 ]; then
     rm -rf $install_dir/1panel/resource/apps/local/napcat
