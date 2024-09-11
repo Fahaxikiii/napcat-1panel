7da563c968f652c18422bcdf029656b00ae77c1a
万里 <920113370213@njust.edu.cn>
2024-09-11 13:54:53 +0800

    Update archiveproxy.sh

--- scripts/archiveproxy.sh
+++ scripts/archiveproxy.sh
@@ -4,13 +4,13 @@ install_dir=$(which 1pctl | xargs grep '^BASE_DIR=' | cut -d'=' -f2)
 
 rm -rf $install_dir/1panel/resource/apps/local/napcat-1panel-napcat
 
-curl -o $install_dir/1panel/resource/apps/local/napcat-1panel-napcat.zip https://github.com/Fahaxikiii/napcat-1panel/archive/refs/heads/napcat.zip
-
-if [ $? -eq 0 ]; then
-    unzip $install_dir/1panel/resource/apps/local/napcat-1panel-napcat.zip
-    rm -rf $install_dir/1panel/resource/apps/local/napcat-1panel-napcat.zip
-    rm -rf $install_dir/1panel/resource/apps/local/napcat
-    mv $install_dir/1panel/resource/apps/local/napcat-1panel-napcat $install_dir/1panel/resource/apps/local/napcat
+if command -v wget > /dev/null; then
+    wget -O $install_dir/1panel/resource/apps/local/napcat-1panel-napcat.zip https://github.com/Fahaxikiii/napcat-1panel/archive/refs/heads/napcat.zip
 else
-    exit 1
+    curl -o $install_dir/1panel/resource/apps/local/napcat-1panel-napcat.zip https://github.com/Fahaxikiii/napcat-1panel/archive/refs/heads/napcat.zip
 fi
+
+unzip $install_dir/1panel/resource/apps/local/napcat-1panel-napcat.zip
+rm -rf $install_dir/1panel/resource/apps/local/napcat-1panel-napcat.zip
+rm -rf $install_dir/1panel/resource/apps/local/napcat
+mv $install_dir/1panel/resource/apps/local/napcat-1panel-napcat $install_dir/1panel/resource/apps/local/napcat
