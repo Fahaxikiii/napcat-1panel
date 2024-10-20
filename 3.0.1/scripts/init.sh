d13456c8ed80f48d8b8657f40f1b7ac8197406e4
万里 <920113370213@njust.edu.cn>
2024-10-20 23:15:25 +0800

    Update init.sh

--- 3.0.1/scripts/init.sh
+++ 3.0.1/scripts/init.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 source ./.env
+
 mkdir -p "${CONFIG_PATH}"
 touch "${CONFIG_PATH}/webui.json"
 WEBUI_PATH="${CONFIG_PATH}/webui.json"
@@ -13,10 +14,16 @@ cat <<EOF > "$WEBUI_PATH"
     "loginRate": ${WEBUI_LOGIN_RATE}
 }
 EOF
+
+touch "${CONFIG_PATH}/config.json"
 PACKET_PATH="${CONFIG_PATH}/config.json"
 cat <<EOF > "$PACKET_PATH"
 {"ip": "127.0.0.1", "port": ${PACKET_PORT}}
 EOF
+
+touch "${CONFIG_PATH}/napcat_${QQ_ACCOUNT}.json"
+NAPCAT_PATH="${CONFIG_PATH}/napcat_${QQ_ACCOUNT}.json"
+cat <<EOF > "$NAPCAT_PATH"
 {
   "fileLog": true,
   "consoleLog": true,
@@ -24,3 +31,4 @@ EOF
   "consoleLogLevel": "info",
   "packetServer": "127.0.0.1:${PACKET_PORT}"
 }
+EOF
