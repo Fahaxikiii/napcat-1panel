9a6f04de59cea7288da7f5613e8e2f50a3f515bc
万里 <920113370213@njust.edu.cn>
2024-11-06 13:45:38 +0800

    update

--- 3.6.0/scripts/init.sh
+++ 3.6.0/scripts/init.sh
@@ -15,21 +15,3 @@ cat <<EOF > "$WEBUI_PATH"
     "loginRate": ${WEBUI_LOGIN_RATE}
 }
 EOF
-
-touch "${CONFIG_PATH}/config.json"
-PACKET_PATH="${CONFIG_PATH}/config.json"
-cat <<EOF > "$PACKET_PATH"
-{"ip": "127.0.0.1", "port": ${PACKET_PORT}}
-EOF
-
-touch "${CONFIG_PATH}/napcat_${QQ_ACCOUNT}.json"
-NAPCAT_PATH="${CONFIG_PATH}/napcat_${QQ_ACCOUNT}.json"
-cat <<EOF > "$NAPCAT_PATH"
-{
-  "fileLog": true,
-  "consoleLog": true,
-  "fileLogLevel": "debug",
-  "consoleLogLevel": "info",
-  "packetServer": "127.0.0.1:${PACKET_PORT}"
-}
-EOF
