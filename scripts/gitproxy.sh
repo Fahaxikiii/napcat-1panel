91c05f94aee203c55d873801c687da18a287e915
万里 <920113370213@njust.edu.cn>
2024-12-21 11:12:51 +0800

    Update gitproxy.sh

--- scripts/gitproxy.sh
+++ scripts/gitproxy.sh
@@ -4,7 +4,7 @@ install_dir=$(which 1pctl | xargs grep '^BASE_DIR=' | cut -d'=' -f2)
 
 rm -rf $install_dir/1panel/resource/apps/local/napcat-1panel-napcat
 
-git clone -b napcat https://ghp.ci/https://github.com/Fahaxikiii/napcat-1panel.git "$install_dir/1panel/resource/apps/local/napcat-1panel-napcat"
+git clone -b napcat https://www.ghproxy.cn/https://github.com/Fahaxikiii/napcat-1panel.git "$install_dir/1panel/resource/apps/local/napcat-1panel-napcat"
 
 if [ $? -eq 0 ]; then
     rm -rf $install_dir/1panel/resource/apps/local/napcat
