3de75dd5e84bbe16b9cabd4a3d39db39b75378de
万里 <920113370213@njust.edu.cn>
2025-01-31 20:06:23 +0800

    Update archive.sh

--- scripts/archive.sh
+++ scripts/archive.sh
@@ -5,9 +5,9 @@ install_dir=$(which 1pctl | xargs grep '^BASE_DIR=' | cut -d'=' -f2)
 rm -rf $install_dir/1panel/resource/apps/local/napcat-1panel-napcat
 
 if command -v wget > /dev/null; then
-    wget -O $install_dir/1panel/resource/apps/local/napcat-1panel-napcat.zip https://gh.spoli.cn/https://github.com/Fahaxikiii/napcat-1panel/archive/refs/heads/napcat.zip
+    wget -O $install_dir/1panel/resource/apps/local/napcat-1panel-napcat.zip https://github.com/Fahaxikiii/napcat-1panel/archive/refs/heads/napcat.zip
 else
-    curl -o $install_dir/1panel/resource/apps/local/napcat-1panel-napcat.zip https://gh.spoli.cn/https://github.com/Fahaxikiii/napcat-1panel/archive/refs/heads/napcat.zip
+    curl -o $install_dir/1panel/resource/apps/local/napcat-1panel-napcat.zip https://github.com/Fahaxikiii/napcat-1panel/archive/refs/heads/napcat.zip
 fi
 
 unzip $install_dir/1panel/resource/apps/local/napcat-1panel-napcat.zip
