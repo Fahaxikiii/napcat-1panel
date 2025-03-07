e777357d4de36feba3a9d2b108b49420452dc669
万里 <920113370213@njust.edu.cn>
2025-03-07 21:01:52 +0800

    Update init.sh

--- 4.6.6/scripts/init.sh
+++ 4.6.6/scripts/init.sh
@@ -9,8 +9,8 @@ WEBUI_PATH="${CONFIG_PATH}/webui.json"
 cat <<EOF > "$WEBUI_PATH"
 {
     "host": "${WEBUI_HOST}",
-    "port": ${PANEL_APP_PORT_HTTP},
     "prefix": "${WEBUI_PREFIX}",
+    "port": ${PANEL_APP_PORT_HTTP},
     "token": "${WEBUI_TOKEN}",
     "loginRate": ${WEBUI_LOGIN_RATE}
 }
